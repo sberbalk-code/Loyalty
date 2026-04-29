d1c82dbc846d169e680fdeb2c67012569d250a24
sberbalk-code <sberbalk@salesforce.com>
2026-04-29 09:59:53 +0200

    Delete src directory

--- src/utils/shuffle.js
+++
@@ -1,9 +0,0 @@
-// Fisher–Yates shuffle. Pure: returns a new array, never mutates the input.
-export function shuffle(input) {
-  const result = [...input]
-  for (let i = result.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1))
-    ;[result[i], result[j]] = [result[j], result[i]]
-  }
-  return result
-}
